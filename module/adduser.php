7f1e109e6314ccb85707540e009a711fa809b79b
Lin,Xi-Zhe <linroex@gmail.com>
2012-07-14 18:48:19 +0800

    修正群組功能的Bug

--- module/adduser.php
+++ module/adduser.php
@@ -13,6 +13,7 @@
 			
 				if($info[4]<=$_SESSION['setting']['total_credit']){
 				$users->insert(array("nickname"=>$info[2],"usernm"=>$info[0],"passwd"=>$info[1],"email"=>$info[3],"day_limit"=>$info[5],"total_limit"=>$info[4],"level"=>$info[6]));
+				$group->insert(array("group_name"=>"未分類","pertain"=>$_SESSION["user-info"]['usernm']));
 				$_SESSION["adduser_result"]="帳號{$info[0]}新增成功";	
 				$_db_setting->update(array('check'=>'999'),array('$set'=>array('total_credit'=>$_SESSION['setting']['total_credit']-$info[4])));
 				$_SESSION['setting']['total_credit']-=$info[4];	
