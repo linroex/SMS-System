99036dcbb397cdcff36f715ca1a6305eef38dc45
Lin,Xi-Zhe <linroex@gmail.com>
2012-07-24 22:57:10 +0800

    Fix Bug

--- module/edit_user.php
+++ module/edit_user.php
@@ -30,23 +30,21 @@
 						$_db_setting->update(array('check'=>'999'),array('$set'=>array('total_credit'=>$_SESSION['setting']['total_credit']+$balance)));
 						$_SESSION['setting']['total_credit']+=$balance;
 						$users->update(array("_id"=>$_id),array('$set'=>array("usernm"=>$info[0],"passwd"=>$info[1],"nickname"=>$info[2],"email"=>$info[3],"day_limit"=>$info[5],"total_limit"=>$info[4],"level"=>$info[6],'phone'=>trim($_POST['phone']))));
-						@$_SESSION['edit_user_status']='修改成功！！';	
+						$_SESSION['edit_user_status']='修改成功！！';	
 					}elseif($old_userinfo['total_limit']<$info[4] and $info[4]>=0 and $_SESSION['setting']['total_credit']>=$balance){
 						$_db_setting->update(array('check'=>'999'),array('$set'=>array('total_credit'=>$_SESSION['setting']['total_credit']-$balance)));
 						$_SESSION['setting']['total_credit']-=$balance;
 						$users->update(array("_id"=>$_id),array('$set'=>array("usernm"=>$info[0],"passwd"=>$info[1],"nickname"=>$info[2],"email"=>$info[3],"total_limit"=>$info[4],"level"=>$info[6],'phone'=>trim($_POST['phone']))));
-						@$_SESSION['edit_user_status']='修改成功！！';	
+						$_SESSION['edit_user_status']='修改成功！！';	
 					}elseif($old_userinfo['total_limit']==$info[4]){
 						
 						$users->update(array("_id"=>$_id),array('$set'=>array("usernm"=>$info[0],"passwd"=>$info[1],"nickname"=>$info[2],"email"=>$info[3],"total_limit"=>$info[4],"level"=>$info[6],'phone'=>trim($_POST['phone']))));
-						@$_SESSION['edit_user_status']='修改成功！！';	
+						$_SESSION['edit_user_status']='修改成功！！';	
 						
-					}else{@$_SESSION['edit_user_status']='系統剩餘點數不足！！';}
+					}else{$_SESSION['edit_user_status']='系統剩餘點數不足！！';}
 				
-				}else{@$_SESSION['edit_user_status']=$info;}	
-			}else{@$_SESSION['edit_user_status']='兩次密碼輸入不相同';}
-			$_SESSION["user-info"]=$users->findOne(array("usernm"=>$_POST["usernm"]));
-			
+				}else{$_SESSION['edit_user_status']=$info;}	
+			}else{$_SESSION['edit_user_status']='兩次密碼輸入不相同';}
 		}
 	}else{
 		$_SESSION['edit_user_status']='檢核碼不正確，可能是編輯時間過久';
